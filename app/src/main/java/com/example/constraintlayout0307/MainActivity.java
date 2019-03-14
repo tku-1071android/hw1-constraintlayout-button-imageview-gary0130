1a51a44c05557cfb61e9dd3cfda090302bfbd4ee
gary0130 <gary880130@gmail.com>
2019-03-14 12:55:06 +0800

    123

--- app/src/main/java/com/example/constraintlayout0307/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout0307/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    int size=10;
     public void cl(View view){
         Toast.makeText(this, "12349", Toast.LENGTH_SHORT).show();
+        TextView hh=findViewById(R.id.txt1);
+        hh.setTextSize(++size);
     }
 }
